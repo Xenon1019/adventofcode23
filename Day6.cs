94bb582a98d8bd1344d4a05b6195d8f530cda05f
Xenon1019 <64680054+Xenon1019@users.noreply.github.com>
2024-11-20 06:00:17 +0000

    Day 6 part 2 done.

--- Day6.cs
+++ Day6.cs
@@ -3,25 +3,25 @@ public class Day6
   public static void Main(string[] args)
   {
     string[] lines = Utility.GetLinesFromFile("./input.txt");
-    long result = 1;
     string[] timeSplit = lines[0].Split(new char[] { ' ', ':' }, StringSplitOptions.RemoveEmptyEntries);
     string[] distanceSplit = lines[1].Split(new char[] { ' ', ':' }, StringSplitOptions.RemoveEmptyEntries);
     if (timeSplit.Length != distanceSplit.Length)
       throw new Exception("Invalid input");
     int count = timeSplit.Length - 1;
-    long num;
+    string timeStr = "", distanceStr = "";
     for (int index = 0; index < count; index++)
     {
-      long t = long.Parse(timeSplit[index + 1]);
-      long d = long.Parse(distanceSplit[index + 1]);
-      double x1 = 0.5 * (t - double.Sqrt(t * t - 4 * d));
-      double x2 = 0.5 * (t + double.Sqrt(t * t - 4 * d));
-      num = 0;
-      if (double.IsInteger(x1)) num--;
-      if (double.IsInteger(x2)) num--;
-      num += 1 + (int)(double.Floor(x2) - double.Ceiling(x1));
-      result *= num;
+      timeStr += timeSplit[index + 1];
+      distanceStr += distanceSplit[index + 1];
     }
+    long t = long.Parse(timeStr);
+    long d = long.Parse(distanceStr);
+    double x1 = 0.5 * (t - double.Sqrt(t * t - 4 * d));
+    double x2 = 0.5 * (t + double.Sqrt(t * t - 4 * d));
+    long result = 0;
+    if (double.IsInteger(x1)) result--;
+    if (double.IsInteger(x2)) result--;
+    result += 1 + (int)(double.Floor(x2) - double.Ceiling(x1));
     Console.WriteLine(result);
   }
 }
